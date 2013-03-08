067278a243597ce3d15400d5ffb58e5d513b604f
Andreas Wagner <Andreas.Wagner@dfki.de>
2013-03-08 10:45:20 +0100

    Release: v0.7.1

--- code/defaults/preferences/firetag.js
+++ code/defaults/preferences/firetag.js
@@ -1,4 +1,4 @@
-pref("extensions.dfki.FireTag.installVersion", "0.7");
+pref("extensions.dfki.FireTag.installVersion", "0.7.1");
 
 pref("extensions.dfki.FireTag.server.host", "projects.dfki.uni-kl.de");
 pref("extensions.dfki.FireTag.server.port", "80");
